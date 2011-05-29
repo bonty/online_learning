ed448c5e9bd9f0d0f5ee1005d1f16ea908700343
bonty <bonty.shushusha@gmail.com>
2011-05-29 23:51:46 +0900

    fix bug

--- perceptron.pl
+++ perceptron.pl
@@ -25,12 +25,14 @@ my $test;
 # params
 my $eta = 1.0;
 my $iter = 20;
-my $method = 'simple';
+my $bias = 0.0;
+my $method = 'P';
 
 GetOptions("train" => \$train,
            "test" => \$test,
            "eta=f" => \$eta,
            "iter=i" => \$iter,
+           "bias=f" => \$bias,
            "method=s" => \$method);
 
 if (!$train && !$test) {
@@ -56,10 +58,10 @@ if ($train) {
     my ($label, @features) = split / /;
     my %fvalue = ();
 
-    # $fvalue{0} = 1;
+    $fvalue{0} = $bias;
     foreach my $val (@features) {
       my ($id, $v) = split /:/, $val;
-      $fvalue{$id} = $v;
+      $fvalue{$id+1} = $v;
       $max_fid = $id if ($id > $max_fid);
     }
 
@@ -70,26 +72,49 @@ if ($train) {
   close $dh;
 
   # initialize weight
-  my @w = (0) x ($max_fid + 1);
-  my @tmp = (0) x ($max_fid + 1);
+  my @w = (0) x ($max_fid + 2);
+  my @tmp = (0) x ($max_fid + 2);
   my $cnt = 0;
 
   for my $i (1 .. $iter) {
     # suffle training data
     shuffleArray(\@expl);
 
-    foreach my $val (@expl) {
-      my $l =  $$val[0];
-      my $fs = $$val[1];
+    my $error = 0;
+    for my $i (0 .. $#expl) {
+      my $l =  $expl[$i][0];
+      my $fs = $expl[$i][1];
       my $score = 0;
 
       foreach my $f (keys %$fs) {
-        $score = $$fs{$f} * $w[$f];
+        $score += $$fs{$f} * $w[$f];
       }
 
       foreach my $f (keys %$fs) {
-        $w[$f] += $eta*$l*$$fs{$f} if ($l * $score <= 0);
+        if ($method eq 'P') {
+          $w[$f] += $eta*$l*$$fs{$f} if ($l * $score <= 0);
+        }
+        elsif ($method eq 'AP1') {
+          if ($l * $score <= 0) {
+            $w[$f] += $eta*$l*$$fs{$f};
+          }
+          else {
+            $tmp[$f] += $w[$f];
+          }
+        }
+        elsif ($method eq 'AP2') {
+          $w[$f] += $eta*$l*$$fs{$f} if ($l * $score <= 0);
+          $tmp[$f] += $w[$f];
+        }
       }
+      $cnt++ if ($l * $score > 0 && $method eq 'AP1');
+      $cnt++ if ($method eq 'AP2');
+    }
+  }
+
+  if ($method eq 'AP1' || $method eq 'AP2') {
+    for my $f (0 .. $#w) {
+      $w[$f] = $tmp[$f] / $cnt;
     }
   }
 
@@ -126,10 +151,12 @@ elsif ($test) {
 
     foreach my $val (@fs) {
       my ($id, $v) = split /:/, $val;
-      $score = $w[$id] * $v if ($w[$id]);
+      $score += $w[$id] * $v if ($w[$id]);
     }
 
     my $estimate = $score >=0 ? "+1" : "-1";
+
+    # print "j:$l\te:$estimate $score\n";
 
     $num++;
     $curr++ if ($l eq $estimate);
